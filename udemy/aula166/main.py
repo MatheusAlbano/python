989d0eb8ca6cbab1fa15862afa85404704a31240
Matheus Albano Porto <135064012+MatheusAlbano@users.noreply.github.com>
2024-08-12 15:19:20 -0300

    Exercicio conta bancaria
    abstração, polimorfismo, herança e encapsulamento

--- udemy/aula166/main.py
+++ udemy/aula166/main.py
@@ -1,8 +1,11 @@
 import varias_linhas
 #import uma_linha
-
+#import documentando_funcoes
+import documentando_class
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(varias_linhas)
+#help(varias_linhas)
+#help(documentando_funcoes)
+help(documentando_class)
